38c5109aabf7ca527023e5f4c21c702cb6113f9b
Sergei Semenov <sergei.semenov@molex.com>
2020-10-01 13:53:16 +0200

    Octave support: pcolor requires float matrix

--- lib/visual_subframeGridGraphic.m
+++ lib/visual_subframeGridGraphic.m
@@ -4,7 +4,8 @@ function visual_subframeGridGraphic( grid )
 
 a = grid; 
 b = [[a nan*zeros(size(a,1),1)] ; nan*zeros(1,size(a,2)+1)];
-pcolor(abs(b)>0); colormap([1 1 1; 1 0 0]);
+map = (abs(b)>0) * 1.0; % float matrix is required by pcolor in Octave
+pcolor(map); colormap([1 1 1; 1 0 0]);
 shading flat
 colormap([1 1 1; 1 0 0]);
 xlabel('SC-FDMA symbol');
