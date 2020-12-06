57a3cb4233ba3fe375ba135c7b4da943d015ccd4
Sergei Semenov <sergey.semionov@gmail.com>
2020-12-06 21:45:06 +0100

    Added CRC check (recover) methods

--- lib/tran_crc16.m
+++ lib/tran_crc16.m
@@ -6,28 +6,47 @@ function [output_bitseq, CRCerror_flg] = tran_crc16( input_bitseq, mode )
 %   mode : 'encoding' for attaching, 'recover' for detecting
 %#codegen
 
-% persistent hCRCEnc;
-% persistent hCRCDet;
+if ((license('test','Communication_Toolbox') == 0) || ...
+    !isempty(getenv('USE_INHOUSE_IMPLEMENTATION')))
+    
+    crc_len = 16;
+    
+    if isequal(mode, 'encode')
+        [crc_bits] = calc_crc(input_bitseq.', 16);
+        output_bitseq = [input_bitseq;crc_bits.'];
+        CRCerror_flg = NaN;
+    elseif isequal(mode, 'recover')
+        [crc_bits] = calc_crc(input_bitseq.', 16);
+              
+        CRCerror_flg = any(crc_bits == 1);
+        
+        output_bitseq = input_bitseq(1:length(input_bitseq) - crc_len);
+    end
+else
+      
+    % persistent hCRCEnc;
+    % persistent hCRCDet;
 
-% if isempty(hCRCEnc)
-    gCRC = zeros(1,16);
-    gCRC(1,17-[16,12,5,0])=1;    
-    hCRCEnc = comm.CRCGenerator('Polynomial', gCRC);
-% end
-% if isempty(hCRCDet)
-    gCRC = zeros(1,16);
-    gCRC(1,17-[16,12,5,0])=1;    
-    hCRCDet = comm.CRCDetector('Polynomial', gCRC);
-% end
+    % if isempty(hCRCEnc)
+        gCRC = zeros(1,16);
+        gCRC(1,17-[16,12,5,0])=1;    
+        hCRCEnc = comm.CRCGenerator('Polynomial', gCRC);
+    % end
+    % if isempty(hCRCDet)
+        gCRC = zeros(1,16);
+        gCRC(1,17-[16,12,5,0])=1;    
+        hCRCDet = comm.CRCDetector('Polynomial', gCRC);
+    % end
 
 
-if isequal(mode, 'encode')
-    output_bitseq = step(hCRCEnc, input_bitseq);
-    CRCerror_flg = NaN;
-elseif isequal(mode, 'recover')
-    [output_bitseq, CRCerror_flg] = step(hCRCDet, input_bitseq);
-end
+    if isequal(mode, 'encode')
+        output_bitseq = step(hCRCEnc, input_bitseq);
+        CRCerror_flg = NaN;
+    elseif isequal(mode, 'recover')
+        [output_bitseq, CRCerror_flg] = step(hCRCDet, input_bitseq);
+    end
 
+end 
 
-end
+end %function [output_bitseq, CRCerror_flg] = tran_crc16( input_bitseq, mode )
 
--- lib/tran_crc24A.m
+++ lib/tran_crc24A.m
@@ -6,28 +6,45 @@ function [output_bitseq, CRCerror_flg] = tran_crc24A( input_bitseq, mode )
 %   mode : 'encoding' for attaching, 'recover' for detecting
 %#codegen
 
-% persistent hCRCEnc;
-% persistent hCRCDet;
+if ((license('test','Communication_Toolbox') == 0) || ...
+    !isempty(getenv('USE_INHOUSE_IMPLEMENTATION')))
 
-% if isempty(hCRCEnc)
-    gCRC = zeros(1,25);
-    gCRC(1,25-[24,23,18,17,14,11,10,7,6,5,4,3,1,0])=1;
-    hCRCEnc = comm.CRCGenerator('Polynomial', gCRC);
-% end
-% if isempty(hCRCDet)
-    gCRC = zeros(1,25);
-    gCRC(1,25-[24,23,18,17,14,11,10,7,6,5,4,3,1,0])=1;
-    hCRCDet = comm.CRCDetector('Polynomial', gCRC);
-% end
+    crc_len = 24;
+
+    if isequal(mode, 'encode')
+        [crc_bits] = calc_crc(input_bitseq.', '24A');
+        output_bitseq = [input_bitseq;crc_bits.'];
+        CRCerror_flg = NaN;
+    elseif isequal(mode, 'recover')
+        [crc_bits] = calc_crc(input_bitseq.', '24A');
+              
+        CRCerror_flg = any(crc_bits == 1);
+        
+        output_bitseq = input_bitseq(1:length(input_bitseq) - crc_len);
+    end
+else
+    % persistent hCRCEnc;
+    % persistent hCRCDet;
+
+    % if isempty(hCRCEnc)
+        gCRC = zeros(1,25);
+        gCRC(1,25-[24,23,18,17,14,11,10,7,6,5,4,3,1,0])=1;
+        hCRCEnc = comm.CRCGenerator('Polynomial', gCRC);
+    % end
+    % if isempty(hCRCDet)
+        gCRC = zeros(1,25);
+        gCRC(1,25-[24,23,18,17,14,11,10,7,6,5,4,3,1,0])=1;
+        hCRCDet = comm.CRCDetector('Polynomial', gCRC);
+    % end
 
 
-if isequal(mode, 'encode')
-    output_bitseq = step(hCRCEnc, input_bitseq);
-    CRCerror_flg = NaN;
-elseif isequal(mode, 'recover')
-    [output_bitseq, CRCerror_flg] = step(hCRCDet, input_bitseq);
-end
+    if isequal(mode, 'encode')
+        output_bitseq = step(hCRCEnc, input_bitseq);
+        CRCerror_flg = NaN;
+    elseif isequal(mode, 'recover')
+        [output_bitseq, CRCerror_flg] = step(hCRCDet, input_bitseq);
+    end
+end 
 
-
-end
+end %function [output_bitseq, CRCerror_flg] = tran_crc24A( input_bitseq, mode )
 
